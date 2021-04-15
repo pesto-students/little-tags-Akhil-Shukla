f1161dcdd1e4e726f2d27486d61fcf34c4f9c127
Akhil Shukla <akhil.shukla@flipkart.com>
2021-04-15 16:24:37 +0530

    initial changes

--- src/components/layouts/MainLayout/MainLayout.jsx
+++ src/components/layouts/MainLayout/MainLayout.jsx
@@ -1,22 +1,34 @@
-import React, {useState} from 'react';
-import Header from '../../Header/Header';
+import React, {useState,useContext} from 'react';
+import Header from '../../header/Header';
 import SignIn from '../../Signin/Signin';
+import FirebaseContext from '../../../firebase/context';
 import './styles.scss';
 
 const MainLayout = ({children}) => {
     const [showSignIn, setShowSignIn] = useState(false);
+    const [showLogo, setShowLogo] =useState(false);
+    const firebase= useContext(FirebaseContext);
 
     const manageSigInModal = () => {
         setShowSignIn(!showSignIn);
     }
 
+    const manageLogo = () => {
+        setShowLogo(!showLogo);
+    }
+
+    const handleSignOut = () => {
+        firebase.doSignOut();
+        manageLogo();
+      };
+
     return(
         <div className={`mainLayout-div layout-${showSignIn}`}>
-            <Header manageSigInModal={manageSigInModal} showSignIn={showSignIn} />
+            <Header manageSigInModal={manageSigInModal} showSignIn={showSignIn} showLogo={showLogo} handleSignOut={handleSignOut} />
             <div className="main">
                 {children}
             </div>
-            {showSignIn?<SignIn manageSigInModal={manageSigInModal} showSignIn={showSignIn} />:null}
+            {showSignIn?<SignIn manageSigInModal={manageSigInModal} showSignIn={showSignIn} manageLogo={manageLogo} />:null}
         </div>
         
     );
--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Hero from '../../components/Hero/Hero';
+
+import Directory from '../../components/Hero/Hero';
 import './styles.scss';
 
 const Homepage = () => {
     return (
-        <section className='home-page-wrapper'>
-           <Hero  />
+        <section className='hero-wrapper'>
+           <Directory  />
         </section>
     )
 }
