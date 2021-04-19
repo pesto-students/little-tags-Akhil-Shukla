df06a85cb8d6c57051e7704e68a27a8a7f643c21
Akhil Shukla <akhil.shukla@flipkart.com>
2021-04-19 18:24:32 +0530

    product desc

--- src/pages/ProductDescriptionPage/ProductDescriptionPage.jsx
+++ src/pages/ProductDescriptionPage/ProductDescriptionPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from '../../components/Footer/Footer';
-import ProductDescription from "../../components/ProductDescription/ProductDescription"
+import ProductDescription from '../../components/ProductDescription/ProductDescription'
 
 
 export default function ProductDescriptionPage(){
